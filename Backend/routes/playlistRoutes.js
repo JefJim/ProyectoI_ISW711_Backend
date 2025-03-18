a0475214edafa9b87c789fda9c147d2134bbfc7d
María José Jiménez <mjimenezmo@est.utn.ac.cr>
2025-03-18 13:12:55 -0600

    adding new routes to playlist routes

--- Backend/routes/playlistRoutes.js
+++ Backend/routes/playlistRoutes.js
@@ -6,8 +6,11 @@ const router = express.Router();
 
 router.use(authMiddleware.authenticate);
 
+
 router.post('/', playlistController.createPlaylist);
 router.get('/', playlistController.getPlaylists);
+router.get('/:id', playlistController.getPlaylistById);
+router.get('/playlist/:id', playlistController.getPlaylistsByRestrictedUser);
 router.put('/:id', playlistController.updatePlaylist);
 router.delete('/:id', playlistController.deletePlaylist);
 
