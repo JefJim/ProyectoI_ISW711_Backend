808af77aedb7d423e640faad97f5200c920a8475
María José Jiménez <mjimenezmo@est.utn.ac.cr>
2025-03-18 13:12:17 -0600

    adding new property to playlist model

--- Backend/models/Playlist.js
+++ Backend/models/Playlist.js
@@ -4,6 +4,7 @@ const playlistSchema = new mongoose.Schema({
     name: { type: String, required: true },
     associatedProfiles: [{ type: mongoose.Schema.Types.ObjectId, ref: 'RestrictedUser' }],
     createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    videos: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Video' }], //New property
 });
 
 module.exports = mongoose.model('Playlist', playlistSchema);
