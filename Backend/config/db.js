9305c18b95273a2d7383da7aff963e80d432c97e
María José Jiménez <mjimenezmo@est.utn.ac.cr>
2025-03-24 01:56:39 -0600

    Add dotenv for environment variables, disable strictQuery in Mongoose, and improve error handling

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+dotenv.config(); // Carga las variables de entorno desde un archivo .env
 const connectDB = async () => {
     try {
+        mongoose.set('strictQuery', false);
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
@@ -9,7 +12,7 @@ const connectDB = async () => {
         console.log('MongoDB connected');
     } catch (error) {
         console.error('MongoDB connection error:', error);
-        process.exit(1);
+        process.exit(1);// detiene la ejecucion si la conexion falla
     }
 };
 
