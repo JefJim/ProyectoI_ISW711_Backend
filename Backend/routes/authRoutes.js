6e25e16ced9b62e8a7ad6cec81e81a9c19e30fb5
JefJim <jefryjimenez2011@gmail.com>
2025-04-22 15:41:57 -0600

    adding google api routes

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const authController = require('../controllers/authController');
+const googleAuthController = require('../controllers/googleAuthController'); //google routes
 
 const router = express.Router();
 
@@ -7,5 +8,9 @@ router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.get('/verify', authController.verifyUser);
 router.post('/verify-code', authController.verifyCode);
+
+//google routes
+router.post('/google', googleAuthController.googleAuth);
+router.post('/google/complete', googleAuthController.completeGoogleRegistration);
 
 module.exports = router;
